e2f4ec49a40d188f331be3d0b53a0ef7d50e06b9
Muhammad Ammarullah R <muhammadammarullah14@gmail.com>
2020-11-23 21:06:12 +0700

    email verification

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -1,14 +1,311 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Verifikasi Pendaftaran Anda</title>
+
+    <style type="text/css">
+        #outlook a {
+            padding: 0;
+        }
+
+        .ReadMsgBody {
+            width: 100%;
+        }
+
+        .ExternalClass {
+            width: 100%;
+        }
+
+        .ExternalClass,
+        .ExternalClass p,
+        .ExternalClass span,
+        .ExternalClass font,
+        .ExternalClass td,
+        .ExternalClass div {
+            line-height: 100%;
+        }
+
+        body,
+        table,
+        td,
+        a {
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+        }
+
+        table,
+        td {
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+        }
+
+        img {
+            -ms-interpolation-mode: bicubic;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
+        }
+
+        table {
+            border-collapse: collapse !important;
+        }
+
+        body {
+            height: 100% !important;
+            margin: 0;
+            padding: 0;
+            width: 100% !important;
+        }
+
+        .appleBody a {
+            color: #68440a;
+            text-decoration: none;
+        }
+
+        .appleFooter a {
+            color: #999999;
+            text-decoration: none;
+        }
+
+        @media screen and (max-width: 525px) {
+
+            table[class="wrapper"] {
+                width: 100% !important;
+            }
+
+            td[class="logo"] {
+                text-align: left;
+                padding: 20px 0 20px 0 !important;
+            }
+
+            td[class="logo"] img {
+                margin: 0 auto !important;
+            }
+
+            td[class="mobile-hide"] {
+                display: none;
+            }
+
+            img[class="mobile-hide"] {
+                display: none !important;
+            }
+
+            img[class="img-max"] {
+                max-width: 100% !important;
+                height: auto !important;
+            }
+
+            table[class="responsive-table"] {
+                width: 100% !important;
+            }
+
+            td[class="padding"] {
+                padding: 10px 5% 15px 5% !important;
+            }
+
+            td[class="padding-copy"] {
+                padding: 10px 5% 10px 5% !important;
+                text-align: center;
+            }
+
+            td[class="padding-meta"] {
+                padding: 30px 5% 0px 5% !important;
+                text-align: center;
+            }
+
+            td[class="no-pad"] {
+                padding: 0 0 20px 0 !important;
+            }
+
+            td[class="no-padding"] {
+                padding: 0 !important;
+            }
+
+            td[class="section-padding"] {
+                padding: 50px 15px 50px 15px !important;
+            }
+
+            td[class="section-padding-bottom-image"] {
+                padding: 50px 15px 0 15px !important;
+            }
+
+            td[class="mobile-wrapper"] {
+                padding: 10px 5% 15px 5% !important;
+            }
+
+            table[class="mobile-button-container"] {
+                margin: 0 auto;
+                width: 100% !important;
+            }
+
+            a[class="mobile-button"] {
+                width: 80% !important;
+                padding: 15px !important;
+                border: 0 !important;
+                font-size: 16px !important;
+            }
+
+        }
+
+    </style>
 </head>
-<body>
-    <h2>Hai, {{ $user->name }}</h2>
-    <p>Terima kasih telah mandaftar pada aplikasi kami</strong></p>
-    <p>Jangan lupa untuk melakukan verifikasi pendaftaran <a href="{{ route('user.verify', $user->activate_token) }}">DISINI</a></p>
+
+<body style="margin: 0; padding: 0;">
+
+    <!-- Some preview text. -->
+    <div style="display: none; max-height: 0; overflow: hidden;">
+
+    </div>
+
+
+    <!-- HEADER -->
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+            <td bgcolor="#ffffff">
+                <div align="center" style="padding: 0px 15px 0px 15px;">
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="500" class="wrapper">
+                        <!-- LOGO/PREHEADER TEXT -->
+                        <tr>
+                            <td style="padding: 20px 0px 30px 0px;" class="logo">
+                                <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+                                    <tr>
+                                        <td bgcolor="#ffffff" width="100" align="center"><a
+                                                href="https://langkaheducation.com/" target="_blank"><img alt="Logo"
+                                                    src="{{ asset('assets/img/logo-primary.svg') }}" width="50%"
+                                                    style="display: block; font-family: Helvetica, Arial, sans-serif; color: #666666; font-size: 16px;"
+                                                    border="0"></a></td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+    <!-- ONE COLUMN SECTION -->
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+            <td bgcolor="#ffffff" align="center" style="padding: 0px 15px 30px 15px;" class="section-padding">
+                <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="500"
+                    class="responsive-table">
+                    <tr>
+                        <td>
+                            <table role="presentation" width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <td>
+                                        <!-- HERO IMAGE -->
+                                        <table role="presentation" width="100%" border="0" cellspacing="0"
+                                            cellpadding="0">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="padding-copy">
+                                                        <table role="presentation" width="100%" border="0"
+                                                            cellspacing="0" cellpadding="0">
+                                                            <tr>
+                                                                <td>
+                                                                    <img src="{{ asset('assets/img/email-verification.svg') }}"
+                                                                        width="500" height="200" border="0"
+                                                                        alt="illustration"
+                                                                        style="display: block; padding: 0; color: #666666; text-decoration: none; font-family: Helvetica, arial, sans-serif; font-size: 16px; width: 500px; height: 200px;"
+                                                                        class="img-max">
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <!-- COPY -->
+                                        <table role="presentation" width="100%" border="0" cellspacing="0"
+                                            cellpadding="0">
+                                            <tr>
+                                                <td align="center"
+                                                    style="font-size: 25px; font-family: Helvetica, Arial, sans-serif; color: #333333; padding-top: 30px;"
+                                                    class="padding-copy">Hai, {{ $user->name }}</td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center"
+                                                    style="padding: 20px 0 0 0; font-size: 16px; line-height: 25px; font-family: Helvetica, Arial, sans-serif; color: #666666;"
+                                                    class="padding-copy">Terima Kasih telah mendaftar. Selamat Bergabung
+                                                    di Langkah Education. Silahkan verifikasi email anda</td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <!-- BULLETPROOF BUTTON -->
+                                        <table role="presentation" width="100%" border="0" cellspacing="0"
+                                            cellpadding="0" class="mobile-button-container">
+                                            <tr>
+                                                <td align="center" style="padding: 25px 0 0 0;" class="padding-copy">
+                                                    <table role="presentation" border="0" cellspacing="0"
+                                                        cellpadding="0" class="responsive-table">
+                                                        <tr>
+                                                            <td align="center"><a
+                                                                    href="{{ route('user.verify', $user->activate_token) }}"
+                                                                    target="_blank"
+                                                                    style="font-size: 16px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #ffffff; text-decoration: none; background-color: #ECB811; border-top: 15px solid #ECB811; border-bottom: 15px solid #ECB811; border-left: 25px solid #ECB811; border-right: 25px solid #ECB811; border-radius: 3px; -webkit-border-radius: 3px; -moz-border-radius: 3px; display: inline-block;"
+                                                                    class="mobile-button">Verifikasi Sekarang &rarr;</a></td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+
+    <!-- FOOTER -->
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+            <td bgcolor="#ffffff" align="center">
+                <table role="presentation" width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+                    <tr>
+                        <td style="padding: 20px 0px 20px 0px;">
+                            <!-- UNSUBSCRIBE COPY -->
+                            <table role="presentation" width="500" border="0" cellspacing="0" cellpadding="0"
+                                align="center" class="responsive-table">
+                                <tr>
+                                    <td align="center" valign="middle"
+                                        style="font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">
+                                        <span>Copyright &copy; Langkah Education 2020</span>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+
 </body>
+
 </html>
