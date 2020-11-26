202d2b2c4b97500623c94c632a059f8079d75bb7
Kukuh KKH <Krahmadani1@gmail.com>
2020-11-27 00:52:26 +0700

    Otentikasi Tryout, Tombol Tryout

--- app/Models/TryoutHasilDetail.php
+++ app/Models/TryoutHasilDetail.php
@@ -7,14 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class TryoutHasilDetail extends Model
 {
     protected $table = 'tryout_hasil_detail';
-    protected $fillable = ['tryout_paket_id', 'tryout_soal_id', 'tryout_kategori_soal_id', 'user_id', 'nilai'];
+    protected $fillable = ['tryout_paket_id', 'tryout_kategori_soal_id', 'tryout_hasil_id', 'user_id', 'nilai'];
 
     public function paket() {
         return $this->belongsTo('App\Models\TryoutPaket', 'tryout_paket_id');
     }
 
-    public function soal() {
-        return $this->belongsTo('App\Models\TryoutSoal', 'tryout_soal_id');
+    public function hasil() {
+        return $this->belongsTo('App\Models\TryoutHasil', 'tryout_paket_id');
+    }
+
+    public function kategori_soal() {
+        return $this->belongsTo('App\Models\TryoutKategoriSoal', 'tryout_kategori_soal_id');
     }
 
     public function user() {
