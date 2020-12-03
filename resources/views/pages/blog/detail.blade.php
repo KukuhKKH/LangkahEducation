c4f2585c3ace9c65e22ccf1c84a68a85bec3af9c
AmmarProjects <muhammadammarullah14@gmail.com>
2020-12-04 00:47:01 +0700

    blog list and single page

--- resources/views/pages/blog/detail.blade.php
+++ resources/views/pages/blog/detail.blade.php
@@ -117,6 +117,15 @@
         background: #ECB811;
     }
 
+    #blog a {
+        color: #aaaaaa;
+        text-decoration: underline;
+    }
+
+    #blog a:hover, #blog a:hover small{
+        color: #ECB811 !important;
+    }
+
 </style>
 @endsection
 
